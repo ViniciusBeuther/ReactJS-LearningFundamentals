d8cdcc2d62d1f31499dc3ec642a612d53eb564d0
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-11-21 12:57:21 -0300

    Update: Conditional render

--- Exercicio4-GeradorDeSenhas/src/App.jsx
+++ Exercicio4-GeradorDeSenhas/src/App.jsx
@@ -1,61 +1,82 @@
-import { useState } from 'react'
-import './App.css'
-import Input from './components/Input';
+import { useState } from "react";
+import "./App.css";
+import Input from "./components/Input";
 
-function generatePassword(size){
-  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*';
+function generatePassword(size) {
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*";
   const lenght = size;
   let i = 0;
-  let password = ''
+  let password = "";
 
-  while( i < lenght){
+  while (i < lenght) {
     let randomNumber = Math.floor(Math.random() * 69);
     password += characters[randomNumber];
     console.log(password);
     i++;
   }
-    return password
+  return password;
 }
 
-function copyToClipboard(password){
-    const passwordInput = document.createElement('textarea');
-    passwordInput.value = password;
-    document.body.appendChild(passwordInput);
-    passwordInput.select();
-    document.execCommand('copy');
-    document.body.removeChild(passwordInput);
+function copyToClipboard(password) {
+  const passwordInput = document.createElement("textarea");
+  passwordInput.value = password;
+  document.body.appendChild(passwordInput);
+  passwordInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(passwordInput);
 }
 
 function App() {
-  const [copyText, setCopyText] = useState("Copy")
-  const [password, setPassword] = useState('')
-  const [passwordSize, setPasswordSize] = useState(12)
-
+  const [copyText, setCopyText] = useState("Copy");
+  const [password, setPassword] = useState("");
+  const [passwordSize, setPasswordSize] = useState(12);
+  const [showInput, setShowInput] = useState(false)
+  
+  const finalPasswordSize = showInput ? passwordSize : 8
   return (
     <>
       <h1>Password Generator</h1>
-      <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+      <label htmlFor="customSize">Custom password size </label>
+
+      <input 
+        type="checkbox" 
+        name="customSize" 
+        id="customSizeCheckbox" 
+        value = {showInput}
+        onChange={() => setShowInput(!showInput)}
+      />
       <br />
+
+    {showInput ? (
+      <div>
+        <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+        <br />
+      </div>
+    ) : null}
+
+      
       <button
         onClick={() => {
-          setPassword(generatePassword(passwordSize))
-          setCopyText('Copy')
+          setPassword(generatePassword(finalPasswordSize));
+          setCopyText("Copy");
         }}
       >
-        Generate a password with {passwordSize} digits
+        Generate a password with {finalPasswordSize} digits
       </button>
 
       <button
-        style={{marginLeft: "15px"}}
+        style={{ marginLeft: "15px" }}
         onClick={() => {
-          setCopyText("Copied")
-          copyToClipboard(password)
-        }}>
-          {copyText}
+          setCopyText("Copied");
+          copyToClipboard(password);
+        }}
+      >
+        {copyText}
       </button>
       <h4>{password}</h4>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Exercicio4-GeradorDeSenhas/src/components/Input.jsx
+++ Exercicio4-GeradorDeSenhas/src/components/Input.jsx
@@ -1,20 +1,21 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 Input.PropTypes = {
-    passwordSize: PropTypes.number.isRequired,
-    setPasswordSize: PropTypes.func
-}
+  passwordSize: PropTypes.number.isRequired,
+  setPasswordSize: PropTypes.func,
+};
 
-export default function Input(props){
-    return(
-        <>
-        <label htmlFor="tamanho">Size: </label>
-        <input
+export default function Input(props) {
+  return (
+    <>
+      <label htmlFor="tamanho">Size: </label>
+      <input
         type="number"
         value={props.passwordSize}
-        onChange={(ev) => props.setPasswordSize(ev.target.value)} 
+        onChange={(ev) => props.setPasswordSize(ev.target.value)}
         style={{
-        marginBottom: '15px'
-        }} />
-        </>
-    )
+          marginBottom: "15px",
+        }}
+      />
+    </>
+  );
 }
