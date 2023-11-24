d3add684411e85eb87026478e6e2837b753645a5
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-11-24 13:05:37 -0300

    Updates

--- ProjetoPratico-1/src/App.jsx
+++ ProjetoPratico-1/src/App.jsx
@@ -1,12 +1,62 @@
+import { useState } from "react"
+
 export default function App(){
-  return(
+  const [games, setGames] = useState([])
+  const [title, setTitle] = useState('')
+  const [cover, setCover] = useState('')
+
+  const handleClick = (ev) => {
+    ev.preventDefault()
+    addGame({ title, cover })
+    setTitle = ''
+    setCover = ''
+  }
+
+  const addGame = ({ title, cover }) => {
+    const id = Math.random(Math.random * 1000)
+    const game = { id, title, cover }
+    setGames(state => [...state, game])
+  }
+
+  return (
     <div id="app">
       <h1>Biblioteca de Jogos</h1>
-      <label htmlFor="game-title">Titulo do jogo: </label>
-      <input type="text" name="game-title" id="title-input" />
+      <form action="form">
+        <label htmlFor="game-title">Titulo do jogo: </label>
+        <input 
+          type="text" 
+          name="game-title" 
+          id="title-input"
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}  
+        />
 
-      <label htmlFor="image-url">Coloque a url da capa: </label>
-      <input type="text" name="image-cover" id="cover-input" />
+        <label htmlFor="image-url">Coloque a url da capa: </label>
+        <input 
+          type="text" 
+          name="image-cover" 
+          id="cover-input"
+          value={cover}
+          onChange={(ev) => setCover(ev.target.value)}
+        />
+
+        <button 
+          onClick={handleClick}
+        >Adicionar</button>
+      </form>
+
+      <div className="games-div">
+        {games.map((item) => (
+          <div key={item.id}>
+            <img src={item.cover} alt="" />
+            <span>
+              <h2>{item.title}</h2>
+              <br />
+              <button>Remover</button>
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
